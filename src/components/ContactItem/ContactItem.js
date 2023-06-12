d39f662f12a13a6dc3e214314fb95eaaa5af08b2
Sergii Podobrii <podobri@ukr.net>
2023-06-12 20:04:59 +0300

    added avatar

--- src/components/ContactItem/ContactItem.js
+++ src/components/ContactItem/ContactItem.js
@@ -1,15 +1,31 @@
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { TiUserDeleteOutline } from 'react-icons/ti';
+import Avatar from '@mui/material/Avatar';
 import { toggleModal } from 'redux/contactsSlice';
 import css from './ContactItem.module.css';
+import { useState } from 'react';
 
 export const ContactItem = ({ id, name, phone, avatar }) => {
   const dispatch = useDispatch();
   const handleToggleModal = () => dispatch(toggleModal(id));
+  const [isValidImageUrl, setIsValidImageUrl] = useState(true);
 
   return (
     <li className={css.contacts__item}>
+      {avatar && isValidImageUrl ? (
+        <img
+          src={avatar}
+          alt="Contact's avatar"
+          width="48"
+          className={css.contacts__img}
+          onError={e => {
+            setIsValidImageUrl(false);
+          }}
+        />
+      ) : (
+        <Avatar />
+      )}
       <p className={css.contacts__data}>
         <span>
           {name}: {phone}
